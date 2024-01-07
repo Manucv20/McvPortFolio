777553704bb9d7300ec3d68e8d4b8d7ad6e855f9
Manuel Cañas <manusevilla20@gmail.com>
2024-01-07 13:39:52 +0100

    fix 404

--- mcv-portfolio/src/pages/404.js
+++ mcv-portfolio/src/pages/404.js
@@ -1,31 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
 
-function NotFound() {
+const NotFound = () => {
   return (
-    <div style={styles.container}>
-      <h1 style={styles.header}>404 - Page Not Found</h1>
-      <p style={styles.paragraph}>Sorry, the page you are looking for does not exist.</p>
-      <p style={styles.paragraph}>
-        You can always go back to the <Link href="/"><a>homepage</a></Link>.
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <p>
+        <Link href="/" className="custom-link">Go to homepage</Link>
       </p>
     </div>
   );
-}
-
-const styles = {
-  container: {
-    textAlign: 'center',
-    paddingTop: '50px',
-  },
-  header: {
-    fontSize: '48px',
-    fontWeight: 'bold',
-  },
-  paragraph: {
-    fontSize: '20px',
-    marginTop: '20px',
-  },
 };
 
 export default NotFound;
